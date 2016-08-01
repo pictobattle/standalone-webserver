2ecda4134f7bb76e4819a63f7effadb5cce99eac
luciengaitskell <luciengaitskell@users.noreply.github.com>
2016-08-01 23:18:23 +0200

    Add a "MONGO_CONFIG" variable in api_v1 for MongoClient arguments

--- api/lib/api_v1.py
+++ api/lib/api_v1.py
@@ -8,11 +8,13 @@ from v1.user import user
 # Static variables
 API_VERSION = 'v1'
 PREFIX = '/' + API_VERSION
+MONGO_CONFIG = {'host': 'localhost', 'post': 27017}
 
 
 class APIv1(object):
     def __init__(self, app):
-        app.config['MONGO_CLIENTv1'] = MongoClient()
+        app.config['MONGO_CLIENTv1'] = MongoClient(MONGO_CONFIG['host'],
+                                                   MONGO_CONFIG['port'])
 
         app.register_blueprint(post, url_prefix=PREFIX + '/post')
 
